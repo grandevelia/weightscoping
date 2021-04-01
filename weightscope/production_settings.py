a10adc9f593758eacf88e34e272ab56efbed1f05
Elias DeVoe <grandevelia@yahoo.com>
2021-03-31 21:33:38 -0500

    add delete account endpoint

--- weightscope/production_settings.py
+++ weightscope/production_settings.py
@@ -28,8 +28,8 @@ EMAIL_HOST_USER = 'apikey'  # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-ALLOWED_HOSTS = 
-    ['weightscoping.herokuapp.com',
+ALLOWED_HOSTS = [
+    'weightscoping.herokuapp.com',
     'https://www.reductiscope.com',
     'exp.host/@grandevelia/reductiscope',
     'expo.io/@grandevelia/reductiscope',
